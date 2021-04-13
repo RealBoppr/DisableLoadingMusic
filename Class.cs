0cf8122899773773c7dabce77c4bda6d0c3e5924
Boppr <crazy_boppr@live.com>
2021-04-13 14:23:26 +1000

    Initial commit

---
+++ Class.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace DisableLoadingMusic
+{
+    public class Class : MelonMod
+    {
+        public override void VRChat_OnUiManagerInit()
+        {
+            GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/LoadingSound").SetActive(false);
+            GameObject.Find("UserInterface/LoadingBackground_TealGradient_Music/LoadingSound").SetActive(false);
+        }
+    }
+}
