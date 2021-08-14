37e2c83c75ad0cfcba37f101f42a102037881d57
Boppr <crazy_boppr@live.com>
2021-08-15 02:08:21 +1000

    :mute:

---
+++ Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(DisableLoadingMusic.Main), "DisableLoadingMusic", "1.1", "Boppr")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace DisableLoadingMusic
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart() => MelonCoroutines.Start(CheckUIManager());
+        private IEnumerator CheckUIManager()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null) { yield return null; }
+            GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/LoadingSound").SetActive(false);
+            GameObject.Find("UserInterface/LoadingBackground_TealGradient_Music/LoadingSound").SetActive(false);
+        }
+    }
+}
